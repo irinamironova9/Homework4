45d06a1157f08d3e01941fd3a5791a0204af2a62
Ирина <mi.irene97@gmail.com>
2022-09-10 21:07:53 +0300

    Готовое Дз 1 к уроку 4

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         // ДЗ 1 Задача 2
         System.out.println("ДЗ 1 Задача 2");
         int personsAge = 25;
-        if (personsAge <7) {
+        if (personsAge < 7) {
             System.out.println("Этот ребёнок ещё не ходит в школу");
         }
         if (personsAge >= 7 && personsAge < 18) {
@@ -28,17 +28,42 @@ public class Main {
 
         // ДЗ 1 Задача 3
         System.out.println("ДЗ 1 Задача 3");
+        // Мой первый вариант решения
+        System.out.println("_Мой первый вариант решения:_");
         int trainsCarCapacity = 102;
         int seats = 60;
-        int passengersNumber = 103;
-        if (passengersNumber <=seats) {
+
+        int passengers = 102; // Кол-во пассажиров, уже купивших билет (при условии, что сначала занимаются сидячие, а потом стоячие места)
+        if (passengers < seats) {
             System.out.println("Проезд возможен, с посадочным местом.");
         }
-        if (passengersNumber > seats && passengersNumber <= trainsCarCapacity) {
+        if (passengers >= seats && passengers < trainsCarCapacity) {
             System.out.println("Проезд возможен, без посадочного места.");
         }
-        if (passengersNumber > trainsCarCapacity) {
+        if (passengers == trainsCarCapacity) {
             System.out.println("Мест нет.");
+        }
+
+        // Вариант решения после просмотра разбора дз:
+        System.out.println("_Вариант решения после просмотра разбора дз:_");
+        int capacity = 102;
+        int seatsCount = 60;
+        int standingCount = capacity - seatsCount;
+
+        int seatsTaken = 60;
+        int standingTaken = 42;
+
+        if (seatsTaken < seatsCount) {
+            System.out.println("Осталось " + (seatsCount - seatsTaken) + " посадочных мест.");
+        }
+        if (seatsTaken == seatsCount) {
+            System.out.println("Посадочных мест нет.");
+        }
+        if (standingTaken < standingCount) {
+            System.out.println("Осталось " + (standingCount - standingTaken) + " стоячих мест.");
+        }
+        if (standingTaken == standingCount) {
+            System.out.println("Стоячих мест нет.");
         }
 
     }
